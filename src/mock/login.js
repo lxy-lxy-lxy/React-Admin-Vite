64731154f1be7cbf2a9a754605522db74cef26d0
xy <123@qq.com>
2024-12-12 18:25:51 +0800

    鉴权功能准备

--- src/mock/login.js
+++ src/mock/login.js
@@ -4,14 +4,12 @@ import md5 from "md5";
 const userList = mock([
     {
         "id": '1',
-        "role_id": "1",
         "username": "admin",
         "password": "123456",
         "token": "123133123",
     },
     {
         "id": '2',
-        "role_id": "2",
         "username": "root",
         "password": "root",
         "token": "65454634",
