4d7dbffca62a069f365ed3e1fcc193c930a7e410
xy <123@qq.com>
2024-12-12 10:40:26 +0800

    输入框Demo

--- src/pages/DevTool/DataTool/FormTool/index.js
+++ src/pages/DevTool/DataTool/FormTool/index.js
@@ -1,253 +1,252 @@
-import React, {useState} from 'react';
-import {
-        Checkbox,
-        Form,
-        Input,
-        Select, Steps, Radio
-} from 'antd';
-import Button from '@components/Button';
+import React, { useState } from "react";
+import { Checkbox, Form, Input, Select, Steps, Radio } from "antd";
+import Button from "@components/Button";
 const { Option } = Select;
 const formItemLayout = {
-        labelCol: {
-                xs: {
-                        span: 24,
-                },
-                sm: {
-                        span: 6,
-                },
-                lg: {
-                        span: 5
-                }
-        },
-        wrapperCol: {
-                xs: {
-                        span: 24,
-                },
-                sm: {
-                        span: 18,
-                },
-                lg: {
-                        span: 19
-                }
-        },
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 6,
+    },
+    lg: {
+      span: 5,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 18,
+    },
+    lg: {
+      span: 19,
+    },
+  },
 };
 const tailFormItemLayout = {
-        wrapperCol: {
-                xs: {
-                        span: 24,
-                        offset: 0,
-                },
-                sm: {
-                        span: 18,
-                        offset: 6,
-                },
-                lg: {
-                        span: 19,
-                        offset: 5,
-                }
-        },
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 18,
+      offset: 6,
+    },
+    lg: {
+      span: 19,
+      offset: 5,
+    },
+  },
 };
 
-const description = 'This is a description.';
+const description = "This is a description.";
 
 const FormTool = () => {
-        const [form] = Form.useForm();
-        const [current, setCurrent] = useState(0);
-        const [formLayout, setFormLayout] = useState('horizontal');
+  const [form] = Form.useForm();
+  const [current, setCurrent] = useState(0);
+  const [formLayout, setFormLayout] = useState("horizontal");
 
-        const onFormLayoutChange = ({ layout }) => {
-                setFormLayout(layout);
-        };
+  const onFormLayoutChange = ({ layout }) => {
+    if (layout) setFormLayout(layout);
+  };
 
-        const onChange = (value) => {
-                console.log('onChange:', current);
-                setCurrent(value);
-        };
+  const onChange = (value) => {
+    console.log("onChange:", current);
+    setCurrent(value);
+  };
 
-        const onFinish = (values) => {
-                console.log('Received values of form: ', values);
-                onChange(1)
-        };
-        const prefixSelector = (
-                <Form.Item name="prefix" noStyle>
-                        <Select
-                                style={{
-                                        width: 70,
-                                }}
-                        >
-                                <Option value="86">+86</Option>
-                                <Option value="87">+87</Option>
-                        </Select>
-                </Form.Item>
-        );
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+    onChange(1);
+  };
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
 
-        const isHorizontal = formLayout === 'horizontal'
+  const isHorizontal = formLayout === "horizontal";
 
-        return (
-                <div style={{maxWidth: "80rem", margin: "auto"}}>
-                        <Steps
-                                className="mb-2"
-                                current={current}
-                                // onChange={onChange}
-                                items={[
-                                        {
-                                                title: 'Step 1',
-                                                description,
-                                        },
-                                        {
-                                                title: 'Step 2',
-                                                description,
-                                        },
-                                        {
-                                                title: 'Step 3',
-                                                description,
-                                        },
-                                ]}
-                        />
-                        <Form
-                                layout={formLayout}
-                                className="mt-2"
-                                colon={false}
-                                {...isHorizontal ? formItemLayout : {}}
-                                form={form}
-                                name="register"
-                                onFinish={onFinish}
-                                initialValues={{
-                                        layout: formLayout,
-                                        residence: ['zhejiang', 'hangzhou', 'xihu'],
-                                        prefix: '86',
-                                }}
-                                onValuesChange={onFormLayoutChange}
-                                scrollToFirstError
-                        >
-                                <Form.Item label="Form Layout" name="layout">
-                                        <Radio.Group value={formLayout}>
-                                                <Radio.Button value="horizontal">Horizontal</Radio.Button>
-                                                <Radio.Button value="vertical">Vertical</Radio.Button>
-                                        </Radio.Group>
-                                </Form.Item>
-                                <Form.Item
-                                        name="email"
-                                        label="E-mail"
-                                        rules={[
-                                                {
-                                                        type: 'email',
-                                                        message: 'The input is not valid E-mail!',
-                                                },
-                                                {
-                                                        required: true,
-                                                        message: 'Please input your E-mail!',
-                                                },
-                                        ]}
-                                >
-                                        <Input />
-                                </Form.Item>
+  return (
+    <div style={{ maxWidth: "80rem", margin: "auto" }}>
+      <Steps
+        className="mb-2"
+        current={current}
+        // onChange={onChange}
+        items={[
+          {
+            title: "Step 1",
+            description,
+          },
+          {
+            title: "Step 2",
+            description,
+          },
+          {
+            title: "Step 3",
+            description,
+          },
+        ]}
+      />
+      <Form
+        layout={formLayout}
+        className="mt-2"
+        colon={false}
+        {...(isHorizontal ? formItemLayout : {})}
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        initialValues={{
+          layout: formLayout,
+          residence: ["zhejiang", "hangzhou", "xihu"],
+          prefix: "86",
+        }}
+        onValuesChange={onFormLayoutChange}
+        scrollToFirstError
+      >
+        <Form.Item label="Form Layout" name="layout">
+          <Radio.Group value={formLayout}>
+            <Radio.Button value="horizontal">Horizontal</Radio.Button>
+            <Radio.Button value="vertical">Vertical</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-                                <Form.Item
-                                        name="password"
-                                        label="Password"
-                                        rules={[
-                                                {
-                                                        required: true,
-                                                        message: 'Please input your password!',
-                                                },
-                                        ]}
-                                        hasFeedback
-                                >
-                                        <Input.Password />
-                                </Form.Item>
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
 
-                                <Form.Item
-                                        name="confirm"
-                                        label="Confirm Password"
-                                        dependencies={['password']}
-                                        hasFeedback
-                                        rules={[
-                                                {
-                                                        required: true,
-                                                        message: 'Please confirm your password!',
-                                                },
-                                                ({ getFieldValue }) => ({
-                                                        validator(_, value) {
-                                                                if (!value || getFieldValue('password') === value) {
-                                                                        return Promise.resolve();
-                                                                }
-                                                                return Promise.reject(new Error('The two passwords that you entered do not match!'));
-                                                        },
-                                                }),
-                                        ]}
-                                >
-                                        <Input.Password />
-                                </Form.Item>
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("The two passwords that you entered do not match!"),
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-                                <Form.Item
-                                        name="nickname"
-                                        label="Nickname"
-                                        tooltip="What do you want others to call you?"
-                                        rules={[
-                                                {
-                                                        required: true,
-                                                        message: 'Please input your nickname!',
-                                                        whitespace: true,
-                                                },
-                                        ]}
-                                >
-                                        <Input />
-                                </Form.Item>
+        <Form.Item
+          name="nickname"
+          label="Nickname"
+          tooltip="What do you want others to call you?"
+          rules={[
+            {
+              required: true,
+              message: "Please input your nickname!",
+              whitespace: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-                                <Form.Item
-                                        name="phone"
-                                        label="Phone Number"
-                                        rules={[
-                                                {
-                                                        required: true,
-                                                        message: 'Please input your phone number!',
-                                                },
-                                        ]}
-                                >
-                                        <Input
-                                                addonBefore={prefixSelector}
-                                                style={{
-                                                        width: '100%',
-                                                }}
-                                        />
-                                </Form.Item>
+        <Form.Item
+          name="phone"
+          label="Phone Number"
+          rules={[
+            {
+              required: true,
+              message: "Please input your phone number!",
+            },
+          ]}
+        >
+          <Input
+            addonBefore={prefixSelector}
+            style={{
+              width: "100%",
+            }}
+          />
+        </Form.Item>
 
-                                <Form.Item
-                                        name="intro"
-                                        label="Intro"
-                                        rules={[
-                                                {
-                                                        required: true,
-                                                        message: 'Please input Intro',
-                                                },
-                                        ]}
-                                >
-                                        <Input.TextArea showCount maxLength={100} />
-                                </Form.Item>
+        <Form.Item
+          name="intro"
+          label="Intro"
+          rules={[
+            {
+              required: true,
+              message: "Please input Intro",
+            },
+          ]}
+        >
+          <Input.TextArea showCount maxLength={100} />
+        </Form.Item>
 
-                                <Form.Item
-                                        name="agreement"
-                                        valuePropName="checked"
-                                        rules={[
-                                                {
-                                                        validator: (_, value) =>
-                                                                value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-                                                },
-                                        ]}
-                                        {...isHorizontal ? tailFormItemLayout : {}}
-                                >
-                                        <Checkbox>
-                                                I have read the <a href="">agreement</a>
-                                        </Checkbox>
-                                </Form.Item>
-                                <Form.Item {...isHorizontal ? tailFormItemLayout : {}}>
-                                        <Button type="primary" htmlType="submit">
-                                                Next
-                                        </Button>
-                                </Form.Item>
-                        </Form>
-                </div>
-        );
+        <Form.Item
+          name="agreement"
+          valuePropName="checked"
+          rules={[
+            {
+              validator: (_, value) =>
+                value
+                  ? Promise.resolve()
+                  : Promise.reject(new Error("Should accept agreement")),
+            },
+          ]}
+          {...(isHorizontal ? tailFormItemLayout : {})}
+        >
+          <Checkbox>
+            I have read the <a href="">agreement</a>
+          </Checkbox>
+        </Form.Item>
+        <Form.Item {...(isHorizontal ? tailFormItemLayout : {})}>
+          <Button type="primary" htmlType="submit">
+            Next
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 export default FormTool;
--- src/services/router.js
+++ src/services/router.js
@@ -1,105 +1,153 @@
-import React, {lazy} from 'react';
-import ErrorPage from '@components/ErrorPage';
-import LoginPage from '../layout/components/Login';
-import App from '../App';
-import {createBrowserRouter, Navigate} from 'react-router-dom';
+import React, { lazy } from "react";
+import ErrorPage from "@components/ErrorPage";
+import LoginPage from "../layout/components/Login";
+import App from "../App";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import {
-        DashboardOutlined,
-        UserOutlined,
-        ToolOutlined
-} from '@ant-design/icons';
-import NProgress from 'nprogress'
+  DashboardOutlined,
+  UserOutlined,
+  ToolOutlined,
+} from "@ant-design/icons";
+import NProgress from "nprogress";
 
-import 'nprogress/nprogress.css'
+import "nprogress/nprogress.css";
 
 const ng = (func) => () => {
-        NProgress.start()
-        return func().then((e) => {
-                NProgress.done()
-                return e
-        })
-}
+  NProgress.start();
+  return func().then((e) => {
+    NProgress.done();
+    return e;
+  });
+};
 
 // 工作台
-const Dashboard = lazy(ng(() => import('@pages/Dashboard')));
+const Dashboard = lazy(ng(() => import("@pages/Dashboard")));
 
 // 配置中心
-const AccountCenter = lazy(ng(() => import('@pages/Setting/AccountCenter')));
-const UserManage = lazy(ng(() => import('@pages/Setting/UserManage')));
-const RoleManage = lazy(ng(() => import('@pages/Setting/RoleManage')));
+const AccountCenter = lazy(ng(() => import("@pages/Setting/AccountCenter")));
+const UserManage = lazy(ng(() => import("@pages/Setting/UserManage")));
+const RoleManage = lazy(ng(() => import("@pages/Setting/RoleManage")));
 
 // DEV工具
-const TableTool = lazy(ng(() => import('@pages/DevTool/TableTool')));
-const EditorTool = lazy(ng(() => import('@pages/DevTool/EditorTool')));
-const ThemeTool = lazy(ng(() => import('@pages/DevTool/ThemeTool')));
-const ButtonTool = lazy(ng(() => import('@pages/DevTool/ButtonTool')));
-const FormTool = lazy(ng(() => import('@pages/DevTool/DataTool/FormTool')));
-const DateTool = lazy(ng(() => import('@pages/DevTool/DataTool/DateTool')));
-const RadioTool = lazy(ng(() => import('@pages/DevTool/DataTool/RadioTool')));
-const CheckTool = lazy(ng(() => import('@pages/DevTool/DataTool/CheckTool')));
+const TableTool = lazy(ng(() => import("@pages/DevTool/TableTool")));
+const EditorTool = lazy(ng(() => import("@pages/DevTool/EditorTool")));
+const ThemeTool = lazy(ng(() => import("@pages/DevTool/ThemeTool")));
+const ButtonTool = lazy(ng(() => import("@pages/DevTool/ButtonTool")));
+const FormTool = lazy(ng(() => import("@pages/DevTool/DataTool/FormTool")));
+const DateTool = lazy(ng(() => import("@pages/DevTool/DataTool/DateTool")));
+const RadioTool = lazy(ng(() => import("@pages/DevTool/DataTool/RadioTool")));
+const CheckTool = lazy(ng(() => import("@pages/DevTool/DataTool/CheckTool")));
+const InputTool = lazy(ng(() => import("@pages/DevTool/DataTool/InputTool")));
 
 const routes = [
-        {
-                path: '/',
-                element: <App/>,
-                // loader: authLoader,
+  {
+    path: "/",
+    element: <App />,
+    // loader: authLoader,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            title: "工作台",
+            icon: <DashboardOutlined />,
+            path: "",
+            children: [{ index: true, title: "首页", element: <Dashboard /> }],
+          },
+          {
+            path: "setting",
+            title: "配置",
+            icon: <UserOutlined />,
+            children: [
+              {
+                path: "/setting/AccountCenter",
+                title: "个人中心",
+                element: <AccountCenter />,
+              },
+              {
+                path: "/setting/UserManage",
+                title: "用户管理",
+                element: <UserManage />,
+              },
+              {
+                path: "/setting/RoleManage",
+                title: "角色管理",
+                element: <RoleManage />,
+              },
+            ],
+          },
+          {
+            path: "devTool",
+            title: "组件",
+            icon: <ToolOutlined />,
+            children: [
+              {
+                path: "/devTool/TableTool",
+                title: "表格",
+                element: <TableTool />,
+              },
+              {
+                path: "/devTool/EditorTool",
+                title: "富文本",
+                element: <EditorTool />,
+              },
+              {
+                path: "/devTool/ThemeTool",
+                title: "主题",
+                element: <ThemeTool />,
+              },
+              {
+                path: "/devTool/ButtonTool",
+                title: "按钮",
+                element: <ButtonTool />,
+              },
+              {
+                path: "/devTool/DataTool",
+                title: "数据录入",
                 children: [
-                        {
-                                errorElement: <ErrorPage/>,
-                                children: [
-                                        {
-                                                title: '工作台',
-                                                icon: <DashboardOutlined/>,
-                                                path: '',
-                                                children: [
-                                                        {index: true, title: '首页', element: <Dashboard/>}
-                                                ]
-                                        },
-                                        {
-                                                path: 'setting',
-                                                title: '配置',
-                                                icon: <UserOutlined/>,
-                                                children: [
-                                                        {path: '/setting/AccountCenter', title: '个人中心', element: <AccountCenter/>},
-                                                        {path: '/setting/UserManage', title: '用户管理', element: <UserManage/>},
-                                                        {path: '/setting/RoleManage', title: '角色管理', element: <RoleManage/>},
-                                                ]
-                                        },
-                                        {
-                                                path: 'devTool',
-                                                title: '组件',
-                                                icon: <ToolOutlined/>,
-                                                children: [
-                                                        {path: '/devTool/TableTool', title: '表格', element: <TableTool/>},
-                                                        {path: '/devTool/EditorTool', title: '富文本', element: <EditorTool/>},
-                                                        {path: '/devTool/ThemeTool', title: '主题', element: <ThemeTool/>},
-                                                        {path: '/devTool/ButtonTool', title: '按钮', element: <ButtonTool/>},
-                                                        {
-                                                                path: '/devTool/DataTool',
-                                                                title: '数据录入',
-                                                                children: [
-                                                                        {path: '/devTool/DataTool/FormTool', title: '表单', element: <FormTool/>},
-                                                                        {path: '/devTool/DataTool/DateTool', title: '日期选择器', element: <DateTool/>},
-                                                                        {path: '/devTool/DataTool/RadioTool', title: '单选框', element: <RadioTool/>},
-                                                                        {path: '/devTool/DataTool/CheckTool', title: '多选框', element: <CheckTool/>},
-                                                                ]
-                                                        }
-                                                ]
-                                        },
-                                        {
-                                                path: '*',
-                                                element: <Navigate to="/" replace={true}/>
-                                        }
-                                ]
-                        }
-                ]
-        },
-        {
-                path: '/login',
-                element: <LoginPage/>
-        }
+                  {
+                    path: "/devTool/DataTool/FormTool",
+                    title: "表单",
+                    element: <FormTool />,
+                  },
+                  {
+                    path: "/devTool/DataTool/DateTool",
+                    title: "日期选择器",
+                    element: <DateTool />,
+                  },
+                  {
+                    path: "/devTool/DataTool/RadioTool",
+                    title: "单选框",
+                    element: <RadioTool />,
+                  },
+                  {
+                    path: "/devTool/DataTool/CheckTool",
+                    title: "多选框",
+                    element: <CheckTool />,
+                  },
+                  {
+                    path: "/devTool/DataTool/InputTool",
+                    title: "输入框",
+                    element: <InputTool />,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            path: "*",
+            element: <Navigate to="/" replace={true} />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
 ];
 
-export {routes};
+export { routes };
 
 export default createBrowserRouter(routes);
