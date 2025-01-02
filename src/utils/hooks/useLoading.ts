d523ac462b2634a7dcec6ab8a08c1e6692ed46fe
xy <123@qq.com>
2025-01-02 13:48:06 +0800

    全局loading逻辑优化

--- src/utils/hooks/useLoading.ts
+++ src/utils/hooks/useLoading.ts
@@ -1,12 +1,31 @@
 import { getLoading } from "@utils/utils.ts";
-import { useMemo } from "react";
+import { useEffect, useState } from "react";
 
 export const useLoading = (loadingKey?: "") => {
   const type = loadingKey ? "comp" : "global";
   const sessionLoading = getLoading(loadingKey, type);
+  const [timer, setTimer] = useState<number | null>(null);
+  const [loading, setLoading] = useState(false);
 
-  const loading =
-    type === "global" ? Object.keys(sessionLoading).length > 0 : sessionLoading;
+  const curLoading =
+    type === "global"
+      ? Object.keys(sessionLoading).length > 0
+      : !!sessionLoading;
 
-  return useMemo(() => loading, [loading]);
+  useEffect(() => {
+    if (curLoading) {
+      if (timer) {
+        clearTimeout(timer);
+        setTimer(null);
+      }
+      setLoading(curLoading);
+    } else {
+      const newTimer = setTimeout(() => {
+        setLoading(curLoading);
+      }, 200);
+      setTimer(newTimer);
+    }
+  }, [curLoading]);
+
+  return loading;
 };
